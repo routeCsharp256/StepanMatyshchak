264ce84031cf3219a69fe10b016252c617807333
steppam <steppam@mail.ru>
2021-11-19 12:14:37 +0300

    remove clothing size from employee

--- src/Domain/AggregationModels/MerchandiseRequest/Employee.cs
+++ src/Domain/AggregationModels/MerchandiseRequest/Employee.cs
@@ -8,17 +8,13 @@ namespace Domain.AggregationModels.MerchandiseRequest
         public Employee(Email email, ClothingSize clothingSize)
         {
             Email = email;
-            ClothingSize = clothingSize;
         }
         
         public Email Email { get; }
-        
-        public ClothingSize ClothingSize { get; }
-        
+                
         protected override IEnumerable<object> GetEqualityComponents()
         {
             yield return Email;
-            yield return ClothingSize;
         }
     }
 }
