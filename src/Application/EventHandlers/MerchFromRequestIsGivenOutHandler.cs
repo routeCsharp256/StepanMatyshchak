525603eeab18314cdbb63b36479490c6a03e8131
steppam <steppam@mail.ru>
2021-12-01 20:39:09 +0300

    add event handler

---
+++ src/Application/EventHandlers/MerchFromRequestIsGivenOutHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Integration;
+using Domain.AggregationModels.MerchandiseRequest.DomainEvents;
+using MediatR;
+
+namespace Application.EventHandlers
+{
+    public class MerchFromRequestIsGivenOutHandler : INotificationHandler<MerchFromRequestIsGivenOut>
+    {
+        private readonly IEmailService _emailService;
+
+        public MerchFromRequestIsGivenOutHandler(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        public async Task Handle(MerchFromRequestIsGivenOut notification, CancellationToken cancellationToken)
+        {
+            await _emailService.SendEmail(notification.Employee.Email,
+                new {Header = "Выдача мерча", Body = "Получите мерч у HR"});
+        }
+    }
+}
